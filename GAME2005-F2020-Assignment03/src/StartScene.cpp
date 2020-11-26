395713e6501741b213c625bacdc5bb2c36771301
lucaskrespi <lucas.krespi@gmail.com>
2020-11-26 12:24:35 -0500

    Start Scene edit

--- GAME2005-F2020-Assignment03/src/StartScene.cpp
+++ GAME2005-F2020-Assignment03/src/StartScene.cpp
@@ -14,6 +14,7 @@ StartScene::~StartScene()
 
 void StartScene::draw()
 {
+	TextureManager::Instance()->draw("background", 0.0f, 0.0f);
 	drawDisplayList();
 }
 
@@ -45,39 +46,42 @@ void StartScene::handleEvents()
 
 void StartScene::start()
 {
+	TextureManager::Instance()->load("../Assets/sprites/backgroundA2.png", "background");
+
+
 	const SDL_Color blue = { 0, 0, 255, 255 };
-	m_pStartLabel = new Label("START SCENE", "Consolas", 80, blue, glm::vec2(400.0f, 40.0f));
+	const SDL_Color strongGreen = { 10, 115, 20, 255 };
+	m_pStartLabel = new Label("THE VACUUM FOREST", "Consolas", 60, strongGreen, glm::vec2(400.0f, 200.0f));
 	m_pStartLabel->setParent(this);
 	addChild(m_pStartLabel);
 
-	m_pInstructionsLabel = new Label("Press 1 to Play", "Consolas", 40, blue, glm::vec2(400.0f, 120.0f));
-	m_pInstructionsLabel->setParent(this);
-	addChild(m_pInstructionsLabel);
+	m_pStudent1 = new Label("Julian Escobar 101286557", "Consolas", 20, strongGreen, glm::vec2(400.0f, 270.0f));
+	m_pStudent1->setParent(this);
+	addChild(m_pStudent1);
 
-
-	m_pShip = new Ship();
-	m_pShip->getTransform()->position = glm::vec2(400.0f, 300.0f); 
-	addChild(m_pShip); 
+	m_pStudent2 = new Label("Lucas Krespi 101289546", "Consolas", 20, strongGreen, glm::vec2(400.0f, 290.0f));
+	m_pStudent2->setParent(this);
+	addChild(m_pStudent2);
 
 	// Start Button
 	m_pStartButton = new Button();
-	m_pStartButton->getTransform()->position = glm::vec2(400.0f, 400.0f); 
+	m_pStartButton->getTransform()->position = glm::vec2(400.0f, 400.0f);
 
 	m_pStartButton->addEventListener(CLICK, [&]()-> void
-	{
-		m_pStartButton->setActive(false);
-		TheGame::Instance()->changeSceneState(BOUNCING_SCENE);
-	});
-	
+		{
+			m_pStartButton->setActive(false);
+			TheGame::Instance()->changeSceneState(PLAY_SCENE);
+		});
+
 	m_pStartButton->addEventListener(MOUSE_OVER, [&]()->void
-	{
-		m_pStartButton->setAlpha(128);
-	});
+		{
+			m_pStartButton->setAlpha(128);
+		});
 
 	m_pStartButton->addEventListener(MOUSE_OUT, [&]()->void
-	{
-		m_pStartButton->setAlpha(255);
-	});
+		{
+			m_pStartButton->setAlpha(255);
+		});
 	addChild(m_pStartButton);
 
 	
